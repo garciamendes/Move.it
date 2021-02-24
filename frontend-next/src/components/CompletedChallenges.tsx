29bd799f9997da850dc99052bfa417f7fdd803a6
garciamendes <garciaprog0101@gmail.com>
2021-02-24 18:16:37 -0300

    application countdown

--- frontend-next/src/components/CompletedChallenges.tsx
+++ frontend-next/src/components/CompletedChallenges.tsx
@@ -3,7 +3,9 @@ import styles from '../styles/components/CompletedChallenges.module.scss'
 
 export function CompletedChallenges() {
   return (
-    <div>
+    <div className={styles.completedChallengesContainer}>
+      <span>Desafios completos</span>
+      <span>5</span>
     </div>
   )
 }
--- frontend-next/src/pages/index.tsx
+++ frontend-next/src/pages/index.tsx
@@ -1,19 +1,30 @@
 // React
 import React from "react";
 
+// Third party
+import Head from 'next/head'
+
 // Local
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import { CompletedChallenges } from "../components/CompletedChallenges";
 import styles from '../styles/pages/Home.module.scss'
+import { Countdown } from "../components/Countdown";
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Move It - Matheus Garcia</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
+          <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
