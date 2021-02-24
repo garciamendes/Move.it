a1f8348c0672004d91fc608923cd9a0c36d54260
garciamendes <garciaprog0101@gmail.com>
2021-02-24 13:39:28 -0300

    migration to Next.js

--- frontend-next/src/components/ExperienceBar.tsx
+++ frontend-next/src/components/ExperienceBar.tsx
@@ -1,12 +1,12 @@
 // Local
-import '../assets/css/experience-bar.scss'
+
 
 export function ExperienceBar() {
   return (
     <header className='experience-bar'>
       <span>0 xp</span>
       <div className='progress-bar'>
-        <div style={{ width: '50%' }}/>
+        <div style={{ width: '50%' }} />
 
         <span className='current-experience' style={{ left: '50%' }}>
           300px
