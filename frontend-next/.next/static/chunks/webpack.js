d091b5c874e38b4c2fbd334b6b77b2208ae2f6cb
garciamendes <garciaprog0101@gmail.com>
2021-02-27 18:51:35 -0300

    to next app

--- frontend-next/.next/static/chunks/webpack.js
+++ frontend-next/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "c7e92053a017b2cc9946";
+/******/ 	var hotCurrentHash = "916f4f49a10577707c64";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
